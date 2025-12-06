bb0eb923dc19b6e7d718151a2c1804e3d6940c8d
LucaChiesa <luca.chiesa@unistra.fr>
2025-12-06 11:44:07 -0500

    Updated everything to Boltz2 from LIT-AlphaFold

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-# Rational design and structural characterization of bioactive molecules - UniMi 2024/2025
-## Protein modelling for drug design using AlphaFold
+# Rational design and structural characterization of bioactive molecules - UniMi 2025/2026
+## Protein modelling for drug design using Boltz2
 
-In this tutorial we will present you how to predict protein structures using AlphaFold2, and how to evalaute the results.
-
-All calculations are performed using the [LIT-AlphaFold](https://github.com/LIT-CCM-lab/LIT-AlphaFold) pipeline, which simplifies input generation and customization compared to the original AlphaFold2 implementation.
+In this tutorial we will present you how to predict protein structures using Boltz2, and how to evalaute the results.
 
 To participate to the tutorial click on this link to the Colab Notebook:
 
@@ -17,7 +15,7 @@ Here we include a few general notes that might be usefull during the tutorial
 
 A Google account is required to access the free computing resources of Colab.
 
-By using Colab you do not need to install anything on your machine, therefore there should be no issue regardless of the used machine.
+By using Colab you do not need to install anything on your machine, therefore there should be no issue regardless of what you have.
 
 #### py3Dmol
 In the tutorial the 3D structure of the generated proteins will be visualized using [py3Dmol](http://3dmol.csb.pitt.edu/).
@@ -27,8 +25,3 @@ To navigate the images the following command might be usefull:
 * Translation: center-click or ctrl+left-click, and move the mouse
 * Zoom: Scrool wheel, or shift+left-click and move the mouse
 * Show/hide atom labels: single left-click (this feature is not universal and is specific for this tutorial)
-
-#### LIT-AlphaFold
-LIT-AlphaFold stores protein information (multiple sequences alignements and templates) as compressed pickle file (*.pkl.bz2*). These file are generally read by the LIT-AlphaFold prediction pipeline to perform calculations, or can be used in python scripts using the package *litaf*.
-
-For more information please visit the [LIT-AlphaFold GitHub repository](https://github.com/LIT-CCM-lab/LIT-AlphaFold).
--- utils.py
+++ utils.py
@@ -5,13 +5,15 @@ import py3Dmol
 import matplotlib.pyplot as plt
 import textwrap
 import numpy as np
+import py3Dmol
 
 nl = '\n'
 
 def align_on_ref(model_file, ref_file, ligname = None, example = True):
     pdb_parser = Bio.PDB.PDBParser(QUIET = True)
+    cif_parser = Bio.PDB.MMCIFParser(QUIET = True)
     ref_structure = pdb_parser.get_structure("reference", ref_file)
-    model_structure = pdb_parser.get_structure("model", model_file)
+    model_structure = cif_parser.get_structure("model", model_file)
 
     ref_seq = ''.join([aa3to1.get(i.resname, None) for i in ref_structure.get_residues() if aa3to1.get(i.resname, None) is not None])
     model_seq = ''.join([aa3to1.get(i.resname, 'X') for i in model_structure.get_residues()])
@@ -85,10 +87,12 @@ def show_aligned(model_file,
                 model_sidecahin_color = 'OrangeCarbon',
                 ref_sidecahin_color = 'OrangeCarbon',
                 width=800,
-                height=600):
+                height=600,
+                extension_model='cif',
+                extension_ref='pdb'):
     view = py3Dmol.view(js='https://3dmol.org/build/3Dmol.js', width = width, height = height)
-    view.addModel(open(model_file,'r').read(),'pdb')
-    view.addModel(open(ref_file,'r').read(),'pdb')
+    view.addModel(open(model_file,'r').read(),extension_model)
+    view.addModel(open(ref_file,'r').read(),extension_refs)
             
     sele = {'resn': 'GST'}
 
@@ -132,4 +136,132 @@ def plot_chain_legend(dpi=100,
              columnspacing=1,
              markerscale=0.5,)
   plt.axis(False)
+  return plt
+
+def plot_msa_v2(msa, sort_lines=True, dpi=100):
+    #Adapted from colabfold
+    seq = msa[0]
+    Ls = [len(seq)]
+    Ln = np.cumsum([0] + Ls)
+    N = len(msa)
+    gap = msa != '-'
+    qid = msa == seq
+    gapid = np.stack([gap[:,Ln[i]:Ln[i+1]].max(-1) for i in range(len(Ls))],-1)
+    lines = []
+    Nn = []
+    for g in np.unique(gapid, axis=0):
+        i = np.where((gapid == g).all(axis=-1))
+        qid_ = qid[i]
+        gap_ = gap[i]
+        seqid = np.stack([qid_[:,Ln[i]:Ln[i+1]].mean(-1) for i in range(len(Ls))],-1).sum(-1) / (g.sum(-1) + 1e-8)
+        non_gaps = gap_.astype(float)
+        non_gaps[non_gaps == 0] = np.nan
+        if sort_lines:
+            lines_ = non_gaps[seqid.argsort()] * seqid[seqid.argsort(),None]
+        else:
+            lines_ = non_gaps[::-1] * seqid[::-1,None]
+        Nn.append(len(lines_))
+        lines.append(lines_)
+
+    Nn = np.cumsum(np.append(0,Nn))
+    lines = np.concatenate(lines,0)
+    plt.figure(figsize=(8,5), dpi=dpi)
+    plt.title("Sequence coverage")
+    plt.imshow(lines,
+              interpolation='nearest', aspect='auto',
+              cmap="rainbow_r", vmin=0, vmax=1, origin='lower',
+              extent=(0, lines.shape[1], 0, lines.shape[0]))
+    for i in Ln[1:-1]:
+        plt.plot([i,i],[0,lines.shape[0]],color="black")
+    for j in Nn[1:-1]:
+        plt.plot([0,lines.shape[1]],[j,j],color="black")
+
+    plt.plot((np.isnan(lines) == False).sum(0), color='black')
+    plt.xlim(0,lines.shape[1])
+    plt.ylim(0,lines.shape[0])
+    plt.colorbar(label="Sequence identity to query")
+    plt.xlabel("Positions")
+    plt.ylabel("Sequences")
+    return plt
+
+
+def show_pdb(pdb_file, n_chains, show_sidechains=False, show_mainchains=False, color="lDDT", extension='pdb'):
+    #The function show_pdb is adapted from ColabFold
+    view = py3Dmol.view(js='https://3dmol.org/build/3Dmol.js',)
+    view.addModel(open(pdb_file,'r').read(), extensions)
+
+    if color == "lDDT":
+        view.setStyle({'cartoon': {'colorscheme': {'prop':'b','gradient': 'roygb','min':50,'max':90}}})
+    elif color == "rainbow":
+        view.setStyle({'cartoon': {'color':'spectrum'}})
+    elif color == "chain":
+        for n,chain,color in zip(range(n_chains),alphabet_list,pymol_color_list):
+           view.setStyle({'chain':chain},{'cartoon': {'color':color}})
+
+    if show_sidechains:
+        BB = ['C','O','N']
+        view.addStyle({'and':[{'resn':["GLY","PRO"],'invert':True},{'atom':BB,'invert':True}]},
+                        {'stick':{'colorscheme':f"WhiteCarbon",'radius':0.3}})
+        view.addStyle({'and':[{'resn':"GLY"},{'atom':'CA'}]},
+                        {'sphere':{'colorscheme':f"WhiteCarbon",'radius':0.3}})
+        view.addStyle({'and':[{'resn':"PRO"},{'atom':['C','O'],'invert':True}]},
+                        {'stick':{'colorscheme':f"WhiteCarbon",'radius':0.3}})
+
+    if show_mainchains:
+        BB = ['C','O','N','CA']
+        view.addStyle({'atom':BB},{'stick':{'colorscheme':f"WhiteCarbon",'radius':0.3}})
+
+    view.setClickable({},
+                        'true',
+                        "function(atom, viewer, event, container){"\
+                        "if(atom.label){viewer.removeLabel(atom.label);delete atom.label;}" \
+                        "else{atom.label=viewer.addLabel(atom.resn+atom.resi+':'+atom.b, {'position': atom});}\n}")
+
+    view.zoomTo()
+    return view
+
+def plot_plddt_legend(dpi=100):
+    #Copied from Colafold
+    thresh = ['plDDT:','Very low (<50)','Low (60)','OK (70)','Confident (80)','Very high (>90)']
+    plt.figure(figsize=(1,0.1),dpi=dpi)
+    ########################################
+    for c in ["#FFFFFF","#FF0000","#FFFF00","#00FF00","#00FFFF","#0000FF"]:
+        plt.bar(0, 0, color=c)
+    plt.legend(thresh, frameon=False,
+             loc='center', ncol=6,
+             handletextpad=1,
+             columnspacing=1,
+             markerscale=0.5,)
+    plt.axis(False)
+    return plt
+
+def plot_plddts(plddts, Ls=None, dpi=100, fig=True):
+  #Copied from Colabfold
+  if fig: plt.figure(figsize=(8,5),dpi=100)
+  plt.title("Predicted lDDT per position")
+  for n,plddt in enumerate(plddts):
+    plt.plot(plddt,label=f"rank_{n+1}")
+  if Ls is not None:
+    L_prev = 0
+    for L_i in Ls[:-1]:
+      L = L_prev + L_i
+      L_prev += L_i
+      plt.plot([L,L],[0,100],color="black")
+  plt.legend()
+  plt.ylim(0,100)
+  plt.ylabel("Predicted lDDT")
+  plt.xlabel("Positions")
+  return plt
+
+def plot_paes(paes, Ls=None, dpi=100, fig=True):
+  #Copied from Colafold
+  num_models = len(paes)
+  if fig: plt.figure(figsize=(3*num_models,2), dpi=dpi)
+  for n,pae in enumerate(paes):
+    plt.subplot(1,num_models,n+1)
+    #plt.title(f"rank_{n+1}")
+    Ln = pae.shape[0]
+    plt.imshow(pae,cmap="bwr",vmin=0,vmax=30,extent=(0, Ln, Ln, 0))
+    if Ls is not None and len(Ls) > 1: plot_ticks(Ls)
+    plt.colorbar()
   return plt
